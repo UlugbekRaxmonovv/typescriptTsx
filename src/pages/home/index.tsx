6664e1ca5e00995a31f15f973765527c92496cbd
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-08-13 20:23:21 +0500

    singleS

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import Product from '../../components/products'
 import CategoryListComponent from '../../components/category'
-import Navbar from '../../components/navbar'
 import Footer from '../../components/footer';
 const Home = () => {
     return (
